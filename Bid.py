7d23536e38dd50426f30fd785a33ae421aaca088
Eun Il Kim <k.eun121@gmail.com>
2017-12-07 21:10:28 -0500

    Misindentation

--- Bid.py
+++ Bid.py
@@ -72,15 +72,15 @@ class Bid:
 		self.id = project_id
 		return 1
 	#creating bid_log
-def add_bid_log(self, time, bidder_id, amount, suggested_time):
-	if time and bidder_id != 'Nan' and amount and suggested_time:
-		if self.bid_log == [[]]:
-			self.bid_log = [[time, bidder_id, amount, suggested_time]]
-		else:
-			(self.bid_log).append([time, bidder_id, amount, suggested_time])
-			jsonIO.set_value(self.db, self.id, "bid_log", self.bid_log)
-		return 1
-	return 0
+	def add_bid_log(self, time, bidder_id, amount, suggested_time):
+		if time and bidder_id != 'Nan' and amount and suggested_time:
+			if self.bid_log == [[]]:
+				self.bid_log = [[time, bidder_id, amount, suggested_time]]
+			else:
+				(self.bid_log).append([time, bidder_id, amount, suggested_time])
+				jsonIO.set_value(self.db, self.id, "bid_log", self.bid_log)
+			return 1
+		return 0
 	def set_bid_log(self, bid_log):
 		if bid_log != [[]] and bid_log and any(isinstance(bid, list) for bid in bid_log):
 			if any(len(bid) != 4 for bid in bid_log):
